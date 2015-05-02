0938ea7650ce022c83dd01be2869e1754e184777
donsalvadori <csalvi42@gmail.com>
2015-05-02 16:06:59 +0000

    initialized app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Notebooker::Application.config.secret_key_base = '4920950061cb62e884ace68d0ec8e5b2ed35fb1a0db94e641692b22544b7c7c796f79a1be3130766d7a3cca3c3255eeb24ab929b01350eab1243d67baccec940'
